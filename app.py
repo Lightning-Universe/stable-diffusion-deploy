27ae2b20ef649e2812be900e0f6dcb7560b1967b
Aniket Maurya <theaniketmaurya@gmail.com>
2022-08-23 15:34:13 +0530

    style

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-import gradio as gr
 import os
+from functools import partial
+
+import gradio as gr
 import lightning as L
 from lightning.app.components.serve import ServeGradio
-from functools import partial
+
+
 class StableDiffusionUI(ServeGradio):
     inputs = [
-        gr.inputs.Textbox(default='cat reading a book', label='Enter the text prompt'),
+        gr.inputs.Textbox(default="cat reading a book", label="Enter the text prompt"),
         gr.Number(value=1, label="Number of images"),
         gr.Slider(value=512, maximum=800, label="Image width"),
-        gr.Slider(value=512, maximum=800, label="Image height")
-        ]
+        gr.Slider(value=512, maximum=800, label="Image height"),
+    ]
     outputs = gr.outputs.Image(type="pil")
     examples = [["golden puppy playing in a pool"], ["cat reading a book"]]
 
@@ -18,6 +21,7 @@ class StableDiffusionUI(ServeGradio):
 
     def build_model(self):
         import os
+
         import torch
         from diffusers import StableDiffusionPipeline
 
@@ -37,7 +41,8 @@ class StableDiffusionUI(ServeGradio):
 
     def predict(self, prompt, num_images, height, width):
         from torch import autocast
-        prompt = [prompt]* int(num_images)
+
+        prompt = [prompt] * int(num_images)
         with autocast("cuda"):
             image = self.model(prompt, height=height, width=width)["sample"][0]
         return image
@@ -47,11 +52,13 @@ class StableDiffusionUI(ServeGradio):
             self._model = self.build_model()
         fn = partial(self.predict, *args, **kwargs)
         fn.__name__ = self.predict.__name__
-        gr.Interface(fn=fn, inputs=self.inputs, outputs=self.outputs, examples=self.examples).launch(
+        gr.Interface(
+            fn=fn, inputs=self.inputs, outputs=self.outputs, examples=self.examples
+        ).launch(
             server_name=self.host,
             server_port=self.port,
             enable_queue=self.enable_queue,
-            title="Stable Diffusion"   
+            title="Stable Diffusion",
         )
 
 
@@ -59,13 +66,13 @@ class RootFlow(L.LightningFlow):
     def __init__(self):
         super().__init__()
         self.model_demo = StableDiffusionUI(cloud_compute=L.CloudCompute("gpu"))
-    
+
     def run(self):
         self.model_demo.run()
-    
+
     def configure_layout(self):
         return [{"name": "Model Demo", "content": self.model_demo}]
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app = L.LightningApp(RootFlow())
