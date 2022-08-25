0e98affcde2398cdc6709e6766b536f93ddb0744
Aniket Maurya <theaniketmaurya@gmail.com>
2022-08-25 10:26:56 +0530

    fixes

--- slack_bot.py
+++ slack_bot.py
@@ -1,3 +1,4 @@
+from ast import arg
 import slack
 from flask import request, Response
 from slackeventsapi import SlackEventAdapter
@@ -22,18 +23,23 @@ client = slack.WebClient(token=os.environ["BOT_TOKEN"])
 
 BOT_ID = client.api_call("auth.test")["user_id"]
 
-def random(channel, title, file):
-    client.files_upload(channels="#dev-bot", title="something random", file="./assets/demo.png")
+def random(data):
+    channel_id = data.get("channel_id")
+    text = data.get("text")
+    print(channel_id, text)
+    client.files_upload(channels=channel_id, title=text, file="./assets/demo.png")
+    # client.files_upload(channels="#dev-bot", title="something random", file="./assets/demo.png")
 
 @app.route("/dream",methods=["post", "get"])
 def dream():
     data = request.form
-    channel = data.get("channel")
+    print(data.keys())
+    channel_id = data.get("channel_id")
     text = data.get("text")
     print(text)
-    th = threading.Thread(target=client.files_upload, kwargs=dict(channels=channel, title=text, file="./assets/demo.png"))
+    # th = threading.Thread(target=client.files_upload, kwargs=dict(channels=channel, title=text, file="./assets/demo.png"))
+    th = threading.Thread(target=random, args=[data])
     th.start()
-    th.join()
     return Response(), 200
 
 
