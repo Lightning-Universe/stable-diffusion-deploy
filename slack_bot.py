8a344d2b3397d8a8d982e06746acf67a39502b8e
Aniket Maurya <theaniketmaurya@gmail.com>
2022-08-25 10:18:17 +0530

    add slack bot

--- slack_bot.py
+++ slack_bot.py
@@ -1,170 +1,41 @@
 import slack
-import os
-from pathlib import Path
-from dotenv import load_dotenv
-from flask import Flask, request, Response
+from flask import request, Response
 from slackeventsapi import SlackEventAdapter
-import string
-from datetime import datetime, timedelta
-import time
+import os
+from flask import Flask
+from concurrent.futures import ThreadPoolExecutor
+import threading
+from flask import send_file
 
-load_dotenv()
+from dotenv import load_dotenv
 
+load_dotenv(".env")
+
+pool = ThreadPoolExecutor()
 app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(
-    os.environ['SIGNING_SECRET_'], '/slack/events', app)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN_'])
-BOT_ID = client.api_call("auth.test")['user_id']
+slack_events_adapter = SlackEventAdapter(os.environ["SIGNING_SECRET"],"/slack/events", app)
 
-message_counts = {}
-welcome_messages = {}
+client = slack.WebClient(token=os.environ["BOT_TOKEN"])
 
-BAD_WORDS = ['hmm', 'no', 'tim']
+# client.chat_postMessage(channel="#dev-bot", text="Hi there!")
 
-SCHEDULED_MESSAGES = [
-    {'text': 'First message', 'post_at': (
-        datetime.now() + timedelta(seconds=20)).timestamp(), 'channel': 'C01BXQNT598'},
-    {'text': 'Second Message!', 'post_at': (
-        datetime.now() + timedelta(seconds=30)).timestamp(), 'channel': 'C01BXQNT598'}
-]
+BOT_ID = client.api_call("auth.test")["user_id"]
 
+def random(channel, title, file):
+    client.files_upload(channels="#dev-bot", title="something random", file="./assets/demo.png")
 
-class WelcomeMessage:
-    START_TEXT = {
-        'type': 'section',
-        'text': {
-            'type': 'mrkdwn',
-            'text': (
-                'Welcome to this awesome channel! \n\n'
-                '*Get started by completing the tasks!*'
-            )
-        }
-    }
-
-    DIVIDER = {'type': 'divider'}
-
-    def __init__(self, channel):
-        self.channel = channel
-        self.icon_emoji = ':robot_face:'
-        self.timestamp = ''
-        self.completed = False
-
-    def get_message(self):
-        return {
-            'ts': self.timestamp,
-            'channel': self.channel,
-            'username': 'Welcome Robot!',
-            'icon_emoji': self.icon_emoji,
-            'blocks': [
-                self.START_TEXT,
-                self.DIVIDER,
-                self._get_reaction_task()
-            ]
-        }
-
-    def _get_reaction_task(self):
-        checkmark = ':white_check_mark:'
-        if not self.completed:
-            checkmark = ':white_large_square:'
-
-        text = f'{checkmark} *React to this message!*'
-
-        return {'type': 'section', 'text': {'type': 'mrkdwn', 'text': text}}
-
-
-def send_welcome_message(channel, user):
-    if channel not in welcome_messages:
-        welcome_messages[channel] = {}
-
-    if user in welcome_messages[channel]:
-        return
-
-    welcome = WelcomeMessage(channel)
-    message = welcome.get_message()
-    response = client.chat_postMessage(**message)
-    welcome.timestamp = response['ts']
-
-    welcome_messages[channel][user] = welcome
-
-
-def list_scheduled_messages(channel):
-    response = client.chat_scheduledMessages_list(channel=channel)
-    messages = response.data.get('scheduled_messages')
-    ids = []
-    for msg in messages:
-        ids.append(msg.get('id'))
-
-    return ids
-
-
-def schedule_messages(messages):
-    ids = []
-    for msg in messages:
-        response = client.chat_scheduleMessage(
-            channel=msg['channel'], text=msg['text'], post_at=msg['post_at']).data
-        id_ = response.get('scheduled_message_id')
-        ids.append(id_)
-
-    return ids
-
-
-def delete_scheduled_messages(ids, channel):
-    for _id in ids:
-        try:
-            client.chat_deleteScheduledMessage(
-                channel=channel, scheduled_message_id=_id)
-        except Exception as e:
-            print(e)
-
-
-def check_if_bad_words(message):
-    msg = message.lower()
-    msg = msg.translate(str.maketrans('', '', string.punctuation))
-
-    return any(word in msg for word in BAD_WORDS)
-
-
-@ slack_event_adapter.on('message')
-def message(payload):
-    event = payload.get('event', {})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
-
-    ts = event.get('ts')
-    client.chat_postMessage(
-        channel=channel_id, thread_ts=ts, text=text)
-
-
-@ slack_event_adapter.on('reaction_added')
-def reaction(payload):
-    event = payload.get('event', {})
-    channel_id = event.get('item', {}).get('channel')
-    user_id = event.get('user')
-
-    if f'@{user_id}' not in welcome_messages:
-        return
-
-    welcome = welcome_messages[f'@{user_id}'][user_id]
-    welcome.completed = True
-    welcome.channel = channel_id
-    message = welcome.get_message()
-    updated_message = client.chat_update(**message)
-    welcome.timestamp = updated_message['ts']
-
-
-@ app.route('/message-count', methods=['POST'])
-def message_count():
+@app.route("/dream",methods=["post", "get"])
+def dream():
     data = request.form
-    user_id = data.get('user_id')
-    channel_id = data.get('channel_id')
-    message_count = message_counts.get(user_id, 0)
+    channel = data.get("channel")
+    text = data.get("text")
+    print(text)
+    th = threading.Thread(target=client.files_upload, kwargs=dict(channels=channel, title=text, file="./assets/demo.png"))
+    th.start()
+    th.join()
+    return Response(), 200
 
-    client.chat_postMessage(
-        channel=channel_id, text=f"Message: {message_count}")
-    return "Hi from Aniket", 200
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True, port=3000)
