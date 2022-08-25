124346e618d16e93e11e3c44bf8974856c2bb1a2
Aniket Maurya <aniket@grid.ai>
2022-08-25 04:40:45 -0400

    update

--- components/slack_api.py
+++ components/slack_api.py
@@ -1,7 +1,10 @@
+import base64
+import json
 import os
 import threading
 from concurrent.futures import ThreadPoolExecutor
 
+import requests
 import slack
 from dotenv import load_dotenv
 from flask import Flask, Response, request
@@ -23,11 +26,25 @@ BOT_ID = client.api_call("auth.test")["user_id"]
 def post_dream(data: dict):
     channel_id = data.get("channel_id")
     prompt = data.get("text")
-    th = threading.Thread(
-        target=client.files_upload,
-        kwargs=dict(channels=channel_id, title=prompt, file="./assets/demo.png"),
-    )
-    th.start()
+    payload = {
+        "data": [
+            prompt,  # represents text of 'Enter the text prompt' Textbox component
+            1,  # represents selected choice of 'Number of images' Radio component
+            512,  # represents selected choice of 'Image Size' Radio component
+        ]
+    }
+    payload = json.dumps(payload)
+    response = requests.post(app.url + "/api/predict", data=payload)
+    print(response.status_code)
+    response.raise_for_status()
+    generated_images: list = response.json()["data"][0]
+
+    with open("generated.png", "w") as fh:
+        image_str = generated_images[0]
+        print(image_str)
+        fh.write(image_str)
+
+    client.files_upload(channels=channel_id, title=prompt, file="./generated.png")
 
 
 @app.route("/dream", methods=["post", "get"])
--- components/slack_command.py
+++ components/slack_command.py
@@ -9,9 +9,10 @@ class SlackCommandWork(L.LightningWork):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def run(self, *args, **kwargs) -> None:
+    def run(self, inference_server_url: str, *args, **kwargs) -> None:
         print("starting Slack Listener")
         print(args)
+        api.url = inference_server_url
         api.run(host=self.host, port=8001)
 
 
