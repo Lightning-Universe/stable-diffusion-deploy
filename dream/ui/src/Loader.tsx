d568c03d16a9917805458cc9b5a5e48297159795
Pritam Soni <pritam@grid.ai>
2022-09-01 22:01:55 +0530

    fixes #18 add loader

---
+++ dream/ui/src/Loader.tsx
@@ -0,0 +1,81 @@
+import { Box, Typography } from '@mui/material';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import { styled } from '@mui/material/styles';
+import { useCallback, useEffect, useState } from 'react';
+
+export const ProgressBar = ({ maxTime = 60 }: { maxTime?: number }) => {
+  const [time, setValue] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setValue(x => x + 1);
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const progressValue = useCallback(() => Math.round(time / (maxTime / 100)), [time, maxTime]);
+  const hasReachedMaximum = useCallback(() => time >= maxTime, [time, maxTime]);
+
+  return (
+    <div>
+      {maxTime > time && <p>Waiting for results, can take upto {maxTime - time} seconds</p>}
+
+      <Box component={'div'} sx={{ width: '400px' }} py={2}>
+        <BorderLinearProgress
+          variant={hasReachedMaximum() ? 'indeterminate' : 'determinate'}
+          value={progressValue()}
+          sx={{ marginBottom: '4px' }}
+        />
+        <Typography component={'span'} {...fontStyles.header}>
+          {hasReachedMaximum() ? `Taking longer than usual, try reloading the page` : `${progressValue()}%`}
+        </Typography>
+        {!hasReachedMaximum() && (
+          <Typography component={'span'} {...fontStyles.body}>
+            {` - ${formatSeconds(maxTime - time)} left`}
+          </Typography>
+        )}
+      </Box>
+    </div>
+  );
+};
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 8,
+  borderRadius: 6,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 6,
+    backgroundColor: theme.palette.primary['main'],
+  },
+}));
+
+const formatSeconds = (seconds: number | undefined) => {
+  if (!seconds) return '';
+  const date = new Date(seconds * 1000);
+  if (seconds <= 60) return `${date.getSeconds()} seconds`;
+  const minutes = date.getMinutes();
+  if (minutes > 1) return `${minutes} minutes and ${date.getSeconds()} seconds`;
+  return `1 minute and ${date.getSeconds()} seconds`;
+};
+
+const fontStyles = {
+  body: {
+    fontFamily: 'Roboto',
+    fontSize: '14px',
+    fontWeight: 400,
+    lineHeight: '20px',
+    color: '#5B5E69',
+  },
+  header: {
+    fontFamily: 'UCity',
+    fontSize: '14px',
+    fontWeight: 600,
+    lineHeight: '20px',
+    minWidth: '60px',
+    color: '#1C1C1C',
+  },
+};
