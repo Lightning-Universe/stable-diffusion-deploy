29f57aa2fd865d3086d8a222a450a5327438e04b
Aniket Maurya <theaniketmaurya@gmail.com>
2022-09-06 11:01:35 +0530

    rename serve module (#31)
    * rename serve module
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- dream/components/__init__.py
+++ dream/components/__init__.py
@@ -1,4 +1,4 @@
 from .dream_slack_bot import DreamSlackCommandBot
-from .stable_diffusion_ui import StableDiffusionServe
+from .stable_diffusion_serve import StableDiffusionServe
 
 __all__ = ["DreamSlackCommandBot", "StableDiffusionServe"]
