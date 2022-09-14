c48b7cfbc6481cd9309c6e2f4f4c2a47a5993e17
Pritam Soni <pritam@grid.ai>
2022-09-14 15:34:28 +0530

    add terms

--- dream/ui/src/App.tsx
+++ dream/ui/src/App.tsx
@@ -1,3 +1,8 @@
+import { Input } from './components/Input';
+import { ProgressBar } from './components/Loader';
+import { AddYourSlackCredentials } from './components/SlackTokensModel';
+import { Typography } from './components/Typography';
+import { postDream } from './services/api';
 import { Box, Container, CssBaseline, Grid, Link, useTheme } from '@mui/material';
 import { EULA } from 'EULA';
 import { useLightningState } from 'hooks/useLightningState';
@@ -7,11 +12,6 @@ import React, { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { LightingState } from 'types/lightning';
-import { Input } from './components/Input';
-import { ProgressBar } from './components/Loader';
-import { AddYourSlackCredentials } from './components/SlackTokensModel';
-import { Typography } from './components/Typography';
-import { postDream } from './services/api';
 
 const queryClient = new QueryClient();
 
@@ -124,8 +124,8 @@ function App() {
       <CssBaseline />
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-        <EULA>
-          <DreamSearch />
+          <EULA>
+            <DreamSearch />
           </EULA>
         </BrowserRouter>
       </QueryClientProvider>
