26e39abdb30740a5c204b9fcb72c7aa490dbcd29
Pritam Soni <23050213+pritamsoni-hsr@users.noreply.github.com>
2022-09-16 20:19:19 +0530

    design revamp (#56)
    * design revamp
    
    * add build files
    
    * rever pre-commit
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- dream/ui/src/App.tsx
+++ dream/ui/src/App.tsx
@@ -1,17 +1,33 @@
-import { Box, Container, CssBaseline, Grid, Link, useTheme } from '@mui/material';
-import { EULA } from 'EULA';
-import { useLightningState } from 'hooks/useLightningState';
-import { Button, Stack } from 'lightning-ui/src/design-system/components';
-import ThemeProvider from 'lightning-ui/src/design-system/theme';
+import {
+  Box,
+  Container,
+  createTheme,
+  CssBaseline,
+  Grid,
+  Link,
+  ThemeProvider as MuiThemeProvider,
+  ToggleButton,
+  ToggleButtonGroup,
+  useTheme,
+} from '@mui/material';
+import { Button, Stack, TextField } from 'lightning-ui/src/design-system/components';
+import { theme } from 'lightning-ui/src/design-system/theme';
 import React, { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
-import { LightingState } from 'types/lightning';
-import { Input } from './components/Input';
+import DreamIcon from './assets/Dream.svg';
+import FlashesIcon from './assets/Flashes.svg';
+import LightningAIIcon from './assets/LightningAI.svg';
+import LogoIcon from './assets/Logo.svg';
+import SlackIcon from './assets/Slack.svg';
+import SpeedIcon from './assets/Speed.svg';
 import { ProgressBar } from './components/Loader';
 import { AddYourSlackCredentials } from './components/SlackTokensModel';
 import { Typography } from './components/Typography';
+import { EULA } from './EULA';
+import { useLightningState } from './hooks/useLightningState';
 import { postDream } from './services/api';
+import { LightingState } from './types/lightning';
 
 const queryClient = new QueryClient();
 
@@ -35,14 +51,7 @@ function Dream({ dream, image }: DreamProps) {
   }
   if (image) {
     return (
-      <img
-        src={image}
-        alt={dream || ''}
-        loading="lazy"
-        width="256px"
-        height="256px"
-        style={{ marginBottom: '40px' }}
-      />
+      <img src={image} alt={dream || ''} loading="lazy" style={{ maxWidth: '320px', width: '100%', height: 'auto' }} />
     );
   }
   return <></>;
@@ -53,6 +62,12 @@ function DreamSearch() {
   const [dream, setDream] = React.useState<string | null>('Cats in hats');
   const [result, setResult] = React.useState<string | null>(null);
   const [requestedDream, setRequestedDream] = React.useState('');
+
+  const [view, setView] = useState<'speed' | 'hd'>('speed');
+  const handleChange = (event: React.MouseEvent<HTMLElement>, nextView: 'speed' | 'hd') => {
+    setView(nextView);
+  };
+
   const [loading, setLoading] = useState(false);
   const dreamIt = async () => {
     if (dream && lightningState) {
@@ -60,8 +75,7 @@ function DreamSearch() {
       setRequestedDream(dream);
       setLoading(true);
       try {
-        //       TODO: update true with toggle button value
-        const result = await postDream(dream, true, lightningState.vars.dream_url);
+        const result = await postDream(dream, view === 'hd', lightningState.vars.dream_url);
         setResult(result[0]);
       } finally {
         setLoading(false);
@@ -74,45 +88,56 @@ function DreamSearch() {
       <Stack direction="column" justifyContent="center" alignItems="center" spacing={3} sx={{ marginTop: '16px' }}>
         <Stack direction="column" alignItems="center" spacing={1} textAlign={'center'}>
           <Box component={'div'} height={16} />
-          <Typography variant="h4" color={theme => theme.palette.primary.main}>
-            Lightning Dream Generator
-          </Typography>
-          <Typography variant="subtitle2" color={theme => theme.palette.primary.main}>
-            Powered by{' '}
-            <Link href={Links.stableDiffusion} target={'_blank'}>
-              Stable Diffusion
-            </Link>
-          </Typography>
-          <Box component={'div'} height={28} />
-
+          <img src={LogoIcon} alt={'app-logo'} width={'90%'} />
+          <Box component={'div'} height={{ xs: 16, sm: 0 }} />
           <Typography variant="subtitle1" color={(theme: any) => theme.palette.grey['70']}>
-            Create beautiful works of art in seconds with the Lightning Dream Generator App.
+            Generate beautiful works of art in seconds!
           </Typography>
-
-          <Box component={'div'} height={16} />
-          <SlackOAuthLink />
         </Stack>
 
-        <Grid container spacing={1} justifyContent="center" alignItems="center">
-          <Grid item xs={12} md>
-            <Input value={dream} onChange={event => setDream(event.target.value)} />
+        <Grid container spacing={1.5} justifyContent="center" alignItems="center" marginTop={'12px !important'}>
+          <Grid item xs={12} marginBottom={{ xs: 1.5, sm: 0 }}>
+            <ToggleButtonGroup exclusive fullWidth orientation={'horizontal'} value={view} onChange={handleChange}>
+              <ToggleButton value="speed" aria-label="speed">
+                <img src={SpeedIcon} alt={'app-logo'} />
+                <Typography ml={1} fontSize={'14px'}>
+                  Super speed
+                </Typography>
+              </ToggleButton>
+              <ToggleButton value="hd" aria-label="hd" sx={{ svg: { color: 'red' } }}>
+                <img src={FlashesIcon} alt={'app-logo'} />
+                <Typography ml={1} fontSize={'14px'}>
+                  High quality
+                </Typography>
+              </ToggleButton>
+            </ToggleButtonGroup>
           </Grid>
 
-          <Grid item sx={{ '.MuiButton-root': { borderRadius: 40, padding: '0 30px' } }}>
-            <Box component={'div'} height={{ xs: 18, md: 0 }} />
-            <Button disabled={!dream || loading} text="Dream it" onClick={dreamIt} />
+          <Grid item xs={12}>
+            <TextField value={dream} onChange={setDream} label={'Type in anything you can imagine'} fullWidth />
           </Grid>
+
+          <Grid item xs={12} sx={{ '.MuiButton-root': { borderRadius: 40, padding: '0 30px' } }}>
+            <Button
+              disabled={!dream || loading}
+              text="Dream it"
+              onClick={dreamIt}
+              fullWidth
+              icon={<img src={DreamIcon} alt={'app-dream'} />}
+            />
+          </Grid>
+
+          {requestedDream && (
+            <Grid item xs={12} textAlign={'center'} my={1.5}>
+              <Dream dream={requestedDream} image={result} />
+            </Grid>
+          )}
 
           <Grid item xs={12} textAlign={'center'}>
+            <SlackOAuthLink />
             <Box component={'div'} height={23} />
-            <Typography variant={'h6'}>
-              <Link href={Links.lightningAI} target={'_blank'}>
-                Built with Lightning AI
-              </Link>
-            </Typography>
           </Grid>
         </Grid>
-        {requestedDream && <Dream dream={requestedDream} image={result} />}
       </Stack>
       {lightningState && <SlackFormAndLicense {...lightningState} />}
     </Container>
@@ -121,7 +146,56 @@ function DreamSearch() {
 
 function App() {
   return (
-    <ThemeProvider>
+    <MuiThemeProvider
+      theme={createTheme({
+        ...theme,
+        shape: {
+          borderRadius: 8,
+        },
+        components: {
+          ...theme.components,
+          MuiLink: {
+            defaultProps: { underline: 'none' },
+          },
+          MuiToggleButton: {
+            styleOverrides: {
+              root: {
+                'border': 'none',
+                'backgroundColor': '#E4E6EB',
+                'img': {
+                  color: 'red',
+                },
+                '&.Mui-selected': {
+                  color: theme.palette.primary.main,
+                  backgroundColor: theme.palette.primary['10'],
+                },
+                '&.Mui-selected:hover': {
+                  color: theme.palette.primary.main,
+                  backgroundColor: theme.palette.primary['20'],
+                },
+                '&.MuiToggleButtonGroup-grouped:not(:last-of-type)': {
+                  borderRadius: '40px 4px 4px 40px',
+                  marginRight: 1,
+                },
+                '&.MuiToggleButtonGroup-grouped:not(:first-of-type)': {
+                  borderRadius: '4px 40px 40px 4px',
+                  marginLeft: 1,
+                },
+              },
+            },
+          },
+          MuiToggleButtonGroup: {
+            defaultProps: {},
+            styleOverrides: {
+              root: {
+                // marginTop: '0',
+                height: '28px',
+                minHeight: '100%',
+              },
+            },
+          },
+        },
+      })}>
       <CssBaseline />
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
@@ -130,7 +204,7 @@ function App() {
           </EULA>
         </BrowserRouter>
       </QueryClientProvider>
-    </ThemeProvider>
+    </MuiThemeProvider>
   );
 }
 
@@ -148,25 +222,56 @@ const SlackFormAndLicense = (lightningState: LightingState) => {
       }}>
       <AddYourSlackCredentials {...lightningState} />
       <br />
-      <Typography variant="caption">
-        <Link href={Links.license} target={'_blank'}>
-          License
-        </Link>
-      </Typography>
+
+      <AppDetailsFooter />
     </Box>
   );
 };
 
+const AppDetailsFooter = () => (
+  <Stack
+    direction={{ xs: 'column', md: 'row' }}
+    alignItems={'center'}
+    justifyContent={'center'}
+    rowGap={{ xs: 2, md: 0 }}>
+    <Typography fontSize={'14px'} fontWeight={600}>
+      Built with{' '}
+      <Link href={Links.lightningAI} target={'_blank'} sx={{ position: 'relative', top: 4 }}>
+        <img src={LightningAIIcon} alt={'LightningAI-logo'} />
+      </Link>
+    </Typography>
+    <Typography color={'#C9CCD1'} fontWeight={200} display={{ xs: 'none', md: 'unset' }}>
+      &nbsp;|&nbsp;
+    </Typography>
+
+    <Stack direction={'row'} alignItems={'center'} justifyContent={'center'}>
+      <Typography fontSize={'14px'} color={theme => theme.palette.text.primary}>
+        Powered by{' '}
+        <Link href={Links.stableDiffusion} target={'_blank'}>
+          Stable Diffusion
+        </Link>
+      </Typography>
+      <Typography color={'#C9CCD1'} fontWeight={200}>
+        &nbsp;|&nbsp;
+      </Typography>
+      <Typography fontSize={'14px'}>
+        <Link href={Links.license} target={'_blank'}>
+          License
+        </Link>
+      </Typography>
+    </Stack>
+  </Stack>
+);
+
 const SlackOAuthLink = () => {
   return (
-    <a href={Links.slack} target={'_blank'} rel="noreferrer">
-      <img
-        alt="Add to Slack"
-        height="40"
-        width="139"
-        src="https://platform.slack-edge.com/img/add_to_slack.png"
-        srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-      />
-    </a>
+    <Link
+      href={Links.slack}
+      target={'_blank'}
+      rel="noreferrer"
+      underline={'none'}
+      sx={{ '.MuiButton-root': { borderRadius: 40 } }}>
+      <Button text={'Add to slack'} icon={<img src={SlackIcon} alt={'app-dream'} />} color={'grey'} fullWidth />
+    </Link>
   );
 };
