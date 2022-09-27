b7abadf76cd6432f073814fefaffcb47e5cff9a3
Kaushik B <kaushikbokka@gmail.com>
2022-09-27 15:19:24 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import find_packages, setup
 _PATH_ROOT = os.path.dirname(__file__)
 
 
-def _load_py_module(fname, pkg="dream"):
+def _load_py_module(fname, pkg="muse"):
     spec = spec_from_file_location(os.path.join(pkg, fname), os.path.join(_PATH_ROOT, pkg, fname))
     py = module_from_spec(spec)
     spec.loader.exec_module(py)
