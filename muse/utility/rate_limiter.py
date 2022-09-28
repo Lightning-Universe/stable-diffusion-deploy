8ba902cd751232cbe843e72b6fac255a7b92b847
Aniket Maurya <theaniketmaurya@gmail.com>
2022-09-28 19:48:11 +0530

    no limit with load test

--- muse/utility/rate_limiter.py
+++ muse/utility/rate_limiter.py
@@ -2,11 +2,14 @@ from typing import List, Tuple
 
 from ratelimit import Rule
 
-from muse.CONST import RATE_LIMIT_KEY
+from muse.CONST import LOAD_TESTING, RATE_LIMIT_KEY
 
 
 # TODO: improve unique user (ip) detection logic. Use Session or JWT
 async def auth_function(scope) -> Tuple[str, str]:
+    if LOAD_TESTING:
+        return "", "load_test"
+
     ip = ""
     if scope["client"]:
         ip, port = tuple(scope["client"])
@@ -31,4 +34,5 @@ RULES: List[Rule] = [
     Rule(second=1, group="default"),  # global unique users
     Rule(minute=10, group="localhost"),  # 127.0.0.1
     Rule(minute=400, group="internal"),  # Slack Bot
+    Rule(group="load_test"),  # No limit when load testing
 ]
