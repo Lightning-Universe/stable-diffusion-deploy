c3a1e0488a91a2b2b00f9dbbe254ff1d21ec5f1d
Pritam Soni <23050213+pritamsoni-hsr@users.noreply.github.com>
2022-09-29 13:26:15 +0530

    add new design

--- muse/ui/src/components/Typography.tsx
+++ muse/ui/src/components/Typography.tsx
@@ -1,7 +1,14 @@
 import { Typography as MuiTypography, TypographyProps } from '@mui/material';
 
-export const Typography = (props: TypographyProps) => {
+export const Typography = ({
+  colorI = 'secondary',
+  ...props
+}: TypographyProps & { colorI?: 'primary' | 'secondary'; fontFamily?: 'Roboto' }) => {
   return (
-    <MuiTypography color={(theme: any) => theme.palette.grey['70']} {...props} style={{ textTransform: 'none' }} />
+    <MuiTypography
+      color={(theme: any) => (colorI === 'primary' ? theme.palette.text.primary : theme.palette.grey['70'])}
+      {...props}
+      style={{ textTransform: 'none' }}
+    />
   );
 };
