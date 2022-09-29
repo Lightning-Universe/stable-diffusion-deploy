c8ba21ad1cc3f01c32cfa99ec47339e16c9bf62c
Kaushik B <45285388+kaushikb11@users.noreply.github.com>
2022-09-29 10:47:42 +0530

    Add Locust Component (#115)

--- muse/components/__init__.py
+++ muse/components/__init__.py
@@ -1,4 +1,6 @@
+from .load_balancer import LoadBalancer
+from .locust import Locust
 from .muse_slack_bot import MuseSlackCommandBot
 from .stable_diffusion_serve import StableDiffusionServe
 
-__all__ = ["MuseSlackCommandBot", "StableDiffusionServe"]
+__all__ = ["MuseSlackCommandBot", "StableDiffusionServe", "Locust", "LoadBalancer"]
