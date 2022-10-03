e1229ccbab8649a9d367621768f4a7a85bd7abca
Kaushik B <kaushikbokka@gmail.com>
2022-10-03 21:49:10 +0530

    Add Safety Checker OpenAI utility

--- muse/components/safety_checker_embedding.py
+++ muse/components/safety_checker_embedding.py
@@ -43,3 +43,31 @@ class SafetyCheckerEmbedding(LightningWork):
         torch.save(embeddings, self.safety_embeddings_filename)
 
         self.drive.put(self.safety_embeddings_filename)
+
+
+class OpenAIBuildConfig(BuildConfig):
+    def build_commands(self) -> List[str]:
+        return ["pip install git+https://github.com/openai/CLIP.git"]
+
+
+class SafetyCheckerOpenAICLIP(LightningWork):
+    def __init__(self, nsfw_list: Optional[List] = None, drive: Optional[Drive] = None):
+        super().__init__(parallel=True, cloud_build_config=OpenAIBuildConfig())
+        self.nsfw_list = nsfw_list or fetch_nsfw_list()
+        self.drive = drive
+        self.safety_embeddings_filename = "safety_embedding.pt"
+
+    def run(self):
+        import clip
+
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        model, _ = clip.load("ViT-B/32", device=device)
+
+        text = clip.tokenize(self.nsfw_list).to(device)
+
+        with torch.no_grad():
+            text_features = model.encode_text(text)
+
+        torch.save(text_features, self.safety_embeddings_filename)
+
+        self.drive.put(self.safety_embeddings_filename)
