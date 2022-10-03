8466bf2964653ed39961cde2d3a5535496d0744c
Kaushik B <kaushikbokka@gmail.com>
2022-10-03 12:56:40 +0530

    Add LightningFlashBuildConfig

--- muse/components/safety_checker_embedding.py
+++ muse/components/safety_checker_embedding.py
@@ -1,16 +1,23 @@
 from typing import List, Optional
 
 import torch
-from lightning import LightningWork
+from lightning import BuildConfig, LightningWork
 from lightning.app.storage import Drive
 from torch import nn
 
 from muse.CONST import NSFW_PREDEFINED_LIST
 
 
+class LightningFlashBuildConfig(BuildConfig):
+    def build_commands(self) -> List[str]:
+        return [
+            "pip install 'git+https://github.com/PyTorchLightning/lightning-flash.git@master#egg=lightning-flash[image,text]'"
+        ]
+
+
 class SafetyCheckerEmbedding(LightningWork):
     def __init__(self, nsfw_list: Optional[List] = None, drive: Optional[Drive] = None):
-        super().__init__(parallel=True)
+        super().__init__(parallel=True, cloud_build_config=LightningFlashBuildConfig())
         self.nsfw_list = nsfw_list or NSFW_PREDEFINED_LIST
         self.drive = drive
         self.safety_embeddings_filename = "safety_embedding.pt"
