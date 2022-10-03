133798aaf14470e99f2e7594b37bbeff6bbcb698
Aniket Maurya <theaniketmaurya@gmail.com>
2022-10-03 13:01:39 +0530

    update

--- muse/db/backend.py
+++ muse/db/backend.py
@@ -4,7 +4,7 @@ sqlite_file_name = "muse.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+engine = create_engine(sqlite_url, echo=False, connect_args=connect_args)
 
 
 def create_db_and_tables():
