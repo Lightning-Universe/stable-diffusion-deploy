0e0b2d76fac75e56848522e914104d7bbce26aca
Aniket Maurya <theaniketmaurya@gmail.com>
2022-10-03 17:43:53 +0530

    update

--- muse/utility/exception_handling.py
+++ muse/utility/exception_handling.py
@@ -1,5 +1,6 @@
 import asyncio
 
+import aiohttp.client_exceptions
 from fastapi import HTTPException
 
 from muse.utility.utils import TimeoutException
@@ -13,11 +14,17 @@ def raise_granular_exception(exception: Exception):
     if isinstance(exception, HTTPException):
         raise exception
 
+    if isinstance(exception, aiohttp.client_exceptions.ServerDisconnectedError):
+        raise HTTPException(500, "Worker server Disconnected")
+
+    if isinstance(exception, aiohttp.client_exceptions.ClientError):
+        raise HTTPException(500, "Worker Server error")
+
     if isinstance(exception, asyncio.TimeoutError):
         raise TimeoutException()
 
     if isinstance(exception, Exception):
         if exception.args[0] == "Server disconnected":
-            raise HTTPException(500, "Model server disconnected")
+            raise HTTPException(500, "Worker server disconnected")
 
     raise HTTPException(500, exception.args[0])
