eca1b5361f3633aec261deb24327b51148b28b30
Pritam Soni <23050213+pritamsoni-hsr@users.noreply.github.com>
2022-10-07 20:38:12 +0530

    remove slack credentials (#160)
    * remove slack credentials
    
    make sure to update the docs to show it
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- muse/ui/src/App.tsx
+++ muse/ui/src/App.tsx
@@ -18,7 +18,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { ReactComponent as FlashesIcon } from './assets/Flashes.svg';
 import MetaImage from './assets/header.png';
 import LogoIcon from './assets/Logo.svg';
-import { AddYourSlackCredentials } from './components/AddYourSlackCredentials';
 import { BuildYourAppBanner } from './components/BuildYourAppBanner';
 import { Footer } from './components/Footer';
 import { OverlayLoader } from './components/OverlayLoader';
@@ -161,7 +160,6 @@ function DreamSearch() {
                 </Box>
               </Row>
               <Box height={8} />
-              {lightningState && <AddYourSlackCredentials {...lightningState} />}
             </Container>
           </Box>
         </Grid>
