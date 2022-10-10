0b184121a8c6e89657dff99ff9ddc6166dcab056
Pritam Soni <23050213+pritamsoni-hsr@users.noreply.github.com>
2022-10-11 00:53:07 +0530

    update copy button to download, and fix layout (#172)

--- muse/ui/src/components/DownloadImageButton.tsx
+++ muse/ui/src/components/DownloadImageButton.tsx
@@ -1,27 +1,15 @@
-import CopyAllRoundedIcon from '@mui/icons-material/CopyAllRounded';
+import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
 import { Box, IconButton } from '@mui/material';
-import { getAndroidVersion } from 'hooks/usePlatform';
-import { useSnackbar } from 'lightning-ui/src/design-system/components';
-import { useMemo } from 'react';
 
-export const DownloadImageButton = (props: { imgResult: string | null }) => {
-  const { enqueueSnackbar } = useSnackbar();
-
+export const DownloadImageButton = (props: { imgResult: string | null; query: string }) => {
   const handleCopyImage = () => {
-    if (!props.imgResult) return;
-    copyImageToClipboard(props.imgResult);
-    enqueueSnackbar({
-      title: 'Image copied to clipboard',
-      severity: 'success',
-    });
+    downloadImage(`${props.query}.png`);
   };
-  const androidVersion = useMemo(() => getAndroidVersion(), []);
 
   const disabled = !props.imgResult;
   return (
     <Box
       sx={{
-        display: androidVersion === 0 ? 'block' : 'none',
         position: 'absolute',
         top: '12px',
         right: '12px',
@@ -31,33 +19,27 @@ export const DownloadImageButton = (props: { imgResult: string | null }) => {
         boxShadow: 'inset 0px 0px 0px 20px rgba(255, 255, 255, 0.3)',
       }}>
       <IconButton onClick={handleCopyImage} disabled={disabled}>
-        <CopyAllRoundedIcon style={{ color: disabled ? '#C5CBD7' : '#1C1C1C' }} />
+        <DownloadRoundedIcon style={{ color: disabled ? '#C5CBD7' : '#1C1C1C' }} />
       </IconButton>
+      {/* only to test, this does not render anything to the screen */}
+      <a href={props.imgResult!} download={props.query} children={''} target={'_blank'} rel={'noreferrer'} />
     </Box>
   );
 };
 
-const copyImageToClipboard = async (content: string) => {
+const downloadImage = async (name: string = 'image.png') => {
   const image = document.getElementById('imgResult') as HTMLImageElement;
-
   if (!image) return;
-
-  const canvas = document.createElement('canvas');
-  canvas.width = image.naturalWidth;
-  canvas.height = image.naturalHeight;
-  const context = canvas.getContext('2d');
-  context!.drawImage(image, 0, 0);
-
-  canvas.toBlob(blob => {
-    if (!blob) return;
-    navigator.clipboard
-      .write([
-        new ClipboardItem({
-          [blob.type]: blob,
-        }),
-      ])
-      .then(() => {
-        console.log('Copied');
-      });
-  });
+  downloadURI(image.src, name);
 };
+
+function downloadURI(uri: string, name: string) {
+  const link = document.createElement('a');
+  link.href = uri;
+  link.download = name;
+
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  link.remove();
+}
