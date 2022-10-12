10dc59f319858f3a853b7337c2e696f16d20dab5
Jirka Borovec <Borda@users.noreply.github.com>
2022-10-12 17:01:02 +0200

    Apply suggestions from code review

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ class APIUsageFlow(L.LightningFlow):
         return APIAccessFrontend(
             apis=[
                 {
-                    "name": "generate images",
+                    "name": "Generate Image",
                     "url": f"{self.api_url}/api/predict",
                     "method": "POST",
                     "request": {"dream": "cats in hats", "high_quality": "true"},
